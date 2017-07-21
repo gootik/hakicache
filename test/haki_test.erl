f6d4b795bce6144fc98cfc21d13fe3b186a83afd
Sasan Hezarkhani <sasan.hezarkhani@vungle.com>
2017-07-21 13:14:00 +0100

    Bucket cache

--- test/haki_test.erl
+++ test/haki_test.erl
@@ -3,11 +3,11 @@
 -include_lib("eunit/include/eunit.hrl").
 
 -record(complex_record_test, {
-    some_list = [1,2,3],
+    some_list = [1, 2, 3],
     map = #{
         a => 'A-OK'
     },
-    tuple = {1,2,3},
+    tuple = {1, 2, 3},
     string = "1234",
     binary = <<"1234">>
 }).
@@ -32,16 +32,33 @@ record_test() ->
     Data = haki:get(test_record_key).
 
 record_large_test() ->
-    Data = [#complex_record_test{} || _ <- lists:seq(1,2000)],
+    Data = [#complex_record_test{} || _ <- lists:seq(1, 2000)],
     haki:cache(test_record_large_key, Data),
     Data = haki:get(test_record_large_key).
 
 record_large_unicode_test() ->
-    Data = [#complex_record_test{binary = <<"∆">>} || _ <- lists:seq(1,2000)],
+    Data = [#complex_record_test{binary = <<"∆">>} || _ <- lists:seq(1, 2000)],
     haki:cache(test_record_large_unicode_key, Data),
     Data = haki:get(test_record_large_unicode_key).
 
 record_huge_unicode_test() ->
-    Data = [#complex_record_test{binary = <<"∆">>} || _ <- lists:seq(1,20000)],
+    Data = [#complex_record_test{binary = <<"∆">>} || _ <- lists:seq(1, 20000)],
     haki:cache(test_record_large_unicode_key, Data),
     Data = haki:get(test_record_large_unicode_key).
+
+bucket_test() ->
+    Data = #{a => some_atom,
+             t => {some, tuple},
+             b => <<"some_binary">>,
+             i => 1234,
+             l => "some_list",
+             m => #{some => map}},
+
+    haki:cache_bucket(unit_test, Data),
+
+    some_atom           = haki:get(unit_test, a),
+    {some, tuple}       = haki:get(unit_test, t),
+    <<"some_binary">>   = haki:get(unit_test, b),
+    1234                = haki:get(unit_test, i),
+    "some_list"         = haki:get(unit_test, l),
+    #{some := map}      = haki:get(unit_test, m).
