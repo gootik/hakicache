b3dec826aac710424d35250aee719121fc63ab1e
Sasan Hezarkhani <gootik_2003@hotmail.com>
2019-01-04 21:39:18 -0800

    OTP 21 (#23)
    * OTP 21
    
    * Backwards compatible

--- src/haki_beam_compiler.erl
+++ src/haki_beam_compiler.erl
@@ -83,11 +83,10 @@ beam_asm(ModName, Val) ->
                {call_ext_only,2,{extfunc,erlang,get_module_info,2}}]}],
             7},
     Chunks = [],
-    Source = atom_to_list(ModName) ++ ".S",
     Opts = ?COMPILER_OPTS,
     CompilerOpts = ?COMPILER_OPTS,
 
-    beam_asm:module(Code, Chunks, Source, Opts, CompilerOpts).
+    call_asm_module(ModName, Code, Chunks, Opts, CompilerOpts).
 
 -spec beam_asm_bucket(cache_module_name(), cache_bucket_value()) -> {ok, binary()} | error.
 beam_asm_bucket(ModName, Map) ->
@@ -153,8 +152,16 @@ beam_asm_bucket(ModName, Map) ->
             NumKeys + 3 + ModuleInfoPadding + 4},
 
     Chunks = [],
-    Source = atom_to_list(ModName) ++ ".S",
     Opts = ?COMPILER_OPTS,
     CompilerOpts = ?COMPILER_OPTS,
 
+    call_asm_module(ModName, Code, Chunks, Opts, CompilerOpts).
+
+-ifdef('OTP_21_PLUS').
+call_asm_module(_ModName, Code, Chunks, Opts, CompilerOpts) ->
+    beam_asm:module(Code, Chunks, Opts, CompilerOpts).
+-else.
+call_asm_module(ModName, Code, Chunks, Opts, CompilerOpts) ->
+    Source = atom_to_list(ModName) ++ ".S",
     beam_asm:module(Code, Chunks, Source, Opts, CompilerOpts).
+-endif.
